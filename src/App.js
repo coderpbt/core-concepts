309137754222c99020cd58740994e20febdca285
coderpbt <90125650+coderpbt@users.noreply.github.com>
2021-09-25 10:26:48 +0600

    hello

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const parson = {
+    name : "Yeasin Arafat",
+    age  : 21,
+    home : "Dinajpur"
+  }
+  const parson2 = {
+    name : "Arafat",
+    age  : 21,
+    home : "Dinajpur"
+  }
+  const style = {
+    background : "#fff",
+    color : "red",
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,7 +23,9 @@ function App() {
         <p>
           Edit 0.1 v <code>src/App.js</code> and save to reload.
         </p>
-
+        <p>My First paragraph</p>
+        <h2 style={style} >Demand: {parson.name+ " " +parson.age+ " " + parson.home}</h2>
+        <h2 style={{backgroundColor:"pink",color:"#fff"}} >Demand: {parson2.name+ " " +parson2.age+ " " + parson2.home}</h2>
       </header>
     </div>
   );
